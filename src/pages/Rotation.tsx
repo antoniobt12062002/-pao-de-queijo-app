fce01926e118921181906e23e6d76f33260cf68e
antoniobt12062002 <antoniotanahashi12@gmail.com>
2026-06-07 19:32:26 -0300

    feat: add routing, ProtectedRoute e app shell

---
+++ src/pages/Rotation.tsx
@@ -0,0 +1,1 @@
+export default function Rotation() { return <div className="p-4">Em construção</div> }
---
+++ src/pages/Scores.tsx
@@ -0,0 +1,1 @@
+export default function Scores() { return <div className="p-4">Em construção</div> }
